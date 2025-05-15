0be831add8ccc727174a3f7dc68c229d22778baa
josephpnp <josephnichollspalis5@gmail.com>
2025-05-15 18:59:21 -0400

    la camara se ve y se guarda el video

--- clas2.py
+++ clas2.py
@@ -1,16 +1,17 @@
 import cv2 as cv 
+import os
 
 capture = cv.VideoCapture(0) # Inicia la captura de video 
-
+salida = cv.VideoWriter("video1.mp4", frameSize=(640,480), fps=20, fourcc=cv.VideoWriter_fourcc(*"mp4v"))
 while (capture.isOpened()): # Si la imagen esta abierta debe iniciar un bucle
     ret, imagen = capture.read() # Lee el frame del stream
-
     if ret == True: #Si hay un frame disponible lo muestra 
         cv.imshow("video", imagen)
-        if cv.waitKey(0) & 0xFF == ord('q'): # Si haces click en q puedes cerrar la ventana
+        salida.write(imagen)
+        if cv.waitKey(1) & 0xFF == ord('q'): # Si haces click en q puedes cerrar la ventana
             break
-        else:
-            break
+    else: break
 capture.release() #libera el proceso de la imagen 
+salida.release()
 cv.destroyAllWindows() #eliminan las ventanas abierta de opencv
-            
+os.system("video1.mp4")
